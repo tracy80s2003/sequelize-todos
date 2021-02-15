bee35bd7f9b3d0348bb43eda16da336932b7d864
Tracy Golden <tracy80s2003@gmail.com>
2021-02-15 16:38:29 -0600

    updated views.js

--- public/js/view.js
+++ public/js/view.js
@@ -15,12 +15,10 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
   const renderTodoList = todos => {
     const todosHTML = todos.map(todo => {
-      
       const completeClass = todo.complete ? 'line-through' : ''
-
       return `<li class="list-group-item todo-item">
         <span class="${completeClass}">${todo.text}</span>
-        <input type="text" class="edit" style="display: none;">
+        <input type="text" data-id="${todo.id}" class="edit" style="display: none;">
         <button data-id="${todo.id}" class="delete btn btn-danger">x</button>
         <button data-id="${todo.id}" data-complete="${todo.complete}" class="complete btn btn-primary">✓</button>
       </li>`
@@ -72,12 +70,25 @@ document.addEventListener('DOMContentLoaded', (e) => {
       deleteTodo(id)
     }else if (target.matches('.complete')) {
       const complete = JSON.parse(target.getAttribute('data-complete'))
-      
       const newTodo = {
         id,
         complete: !complete
       }
+      updateTodo(newTodo)
+    } else if (target.matches('span')) {
+      const input = target.nextElementSibling
+      input.value = target.innerText
+      input.style.display = 'block'
+      target.style.display = 'none'
+    }
+  })
 
+  todoListSpan.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+      const newTodo = {
+        id: e.target.getAttribute('data-id'),
+        text: e.target.value
+      }
       updateTodo(newTodo)
     }
   })
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,47 +3,48 @@ const router = express.Router()
 const db = require('../models')
 
 router.get('/api/todos', async (req, res) => {
-    try {
-        const todos = await db.Todo.findAll()
-        res.json(todos)
-        } catch(err) {
-        res.status(500).send(err)
-    }
+  try {
+    const todos = await db.Todo.findAll()
+    res.json(todos)
+  } catch(err) {
+    res.status(500).send(err)
+  }
 })
 
 router.post('/api/todos', async (req, res) => {
-    try {
-        // pause the code with await and create the record
-        await db.Todo.create(req.body)
-        res.stats(200).send()
-    } catch(err) {
-        res.status(500).send(err)
-    }
+  try {
+    await db.Todo.create(req.body)
+    res.status(200).send()
+  } catch(err) {
+    res.status(500).send(err)
+  }
 })
 
-router.put('/api/todos/:id', async (req,res) => {
-    try {
-        const id = req.params.id
-        // pause the code with "await" and update the record
-        await db.Todo.update(req.body, {
-            where: { id }
-        })
-        res.stats(200).send()
-    } catch(err) {
-        res.status(500).send(err)
-    }
+router.put('/api/todos/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+
+    console.log(req.body)
+
+    await db.Todo.update(req.body, {
+      where: { id }
+    })
+    res.status(200).send()
+  } catch(err) {
+    res.status(500).send(err)
+  }
 })
 
 router.delete('/api/todos/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        await db.Todo.destroy({
-            where: { id }
-        })
-        res.status(200).send()
-    } catch(err) {
-        res.status(500).send(err)
-}
+  try {
+    const id = req.params.id
+    await db.Todo.destroy({
+      where: { id }
+    })
+    res.status(200).send()
+  } catch(err) {
+    res.status(500).send(err)
+  }
 })
 
 module.exports = router
